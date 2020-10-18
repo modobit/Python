b90d92481fdade67e5a127f6801eff421905e2c0
modo <modobit@users.noreply.github.com>
2020-10-17 21:40:39 -0400

    updated

---
+++ playground/functions.py
@@ -0,0 +1,17 @@
+x = 1
+if x == 1:
+    # indented four spaces
+    print("x is 1.")
+
+
+# Variables 
+
+myfloat = 7.0
+print(myfloat)
+myfloat = float(7)
+print(myfloat)
+
+mystring = 'hello'
+print(mystring)
+mystring = "hello"
+print(mystring)
