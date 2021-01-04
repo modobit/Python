29dae76ea1cc03078d37a0881312d1ea7aeaf9bf
modo <modobit@users.noreply.github.com>
2021-01-04 12:13:56 -0500

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,1 +1,14 @@
-# Test script
+# Test Function script
+
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+my_function("Emil", "Tobias", "Linus")
