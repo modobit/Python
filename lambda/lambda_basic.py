c9b897b4e6010f5875671a5921df25019adc56e4
modo <modobit@users.noreply.github.com>
2021-01-04 13:15:38 -0500

    lambda funstions

---
+++ lambda/lambda_basic.py
@@ -0,0 +1,4 @@
+# Lambda Function 
+
+x = lambda a, b : a * b
+print(x(5, 6))
--- main/main.py
+++ main/main.py
@@ -12,3 +12,15 @@ def my_function(*kids):
   print("The youngest child is " + kids[2])
 
 my_function("Emil", "Tobias", "Linus")
+# function call 
+
+
+def my_function(x):
+  return 5 * x
+
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
+
+
+
